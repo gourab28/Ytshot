13d580e11d1ead5d3c559f113ad0d907a761beeb
Gourab Das <vicky.slg100@gmail.com>
2021-06-16 08:44:24 +0530

    digital

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/download', async (req, res) => {
         let filemp = videf.url;
 
         //Response
-        res.status(200).json({title: name , mp3: audioFormats[1].url , videolink: filemp, direct: url})
+        res.status(200).json({title: name , mp3: audioFormats[1].url , videolink: filemp, direct: url , statu: "30"})
     })
     
   app.get('/video/:video', async (req, res) => {
@@ -45,7 +45,7 @@ app.get('/download', async (req, res) => {
         });
         const ttel = encodeURI(title);
          //console.log(ttel);
-        res.header('Content-Disposition', `attachment; filename="${ttel}-Shortsloader.mp4"`);
+        res.header('Content-Disposition', `attachment; filename="${ttel}.mp4"`);
         ytdl(url, {
             format: 'mp4',
             quality: 'highestvideo',
@@ -63,19 +63,18 @@ app.get('/audio/:audio', async (req, res) => {
             return res.sendStatus(400);
         }
         let info = await ytdl.getInfo(url);
+        console.log(info.videoDetails.title);
         const title = slugify(info.videoDetails.title, {
-            replacement: ' ',
-            remove: /[*+~.()'"!:@||#₹&"?!}{=¢$¥€•√π÷×¶∆~`π•√=}]/g,
+            replacement: '-',
+            remove: /[*+~.()'"!:@]/g,
             lower: true,
             strict: false
         });
-        const ttel = encodeURI(title);
-         //console.log(ttel);
-        res.header('Content-Disposition', `attachment; filename="${ttel}-Shortsloader.mp3"`);
+        res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
         ytdl(url, {
             format: 'mp3',
             filter: 'audioonly',
-            quality: 'highest'
+            quality: 'highestaudio'
         }).pipe(res);
 
     } catch (err) {
